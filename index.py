1cbdb367cc88086e74ebfca9c696c7662c73175f
IT_error <87294691+IT-Err0R@users.noreply.github.com>
2021-09-15 08:40:16 +0300

    Update index.py

--- index.py
+++ index.py
@@ -1,17 +1,44 @@
 class Ruby():
 
-    def ruby_print(text):
+    #The Print Function(*Made By Dragovvv*)
+    def ruby_print_txt(text):
 
         script = open("script.rb","a")
-
         script.write(f'print "{text}" ')
-
         script.close()
         
-    def ruby_print_Line(text):
+    #The Puts Function(*Made By IT-Err0r*)
+    def ruby_print_Line_txt(text):
+        
+        script = open("script.rb","a")
+        script.write(f'puts "{text}" ')
+        script.close()
+
+    #The Print Function(*Made By IT-Err0R*)
+    def ruby_print_num(number):
 
         script = open("script.rb","a")
+        script.write(f'print {number} ')
+        script.close()
+        
+    #The Puts Function(*Made By IT-Err0r*)
+    def ruby_print_Line_num(number):
+        
+        script = open("script.rb","a")
+        script.write(f'puts {number} ')
+        script.close()
 
-        script.write(f'puts "{text}" ')
 
+    #The Variable Function(*Made By IT-Err0r*)
+    def ruby_var_int(varname,value: int):
+        
+        script = open("script.rb","a")
+        script.write(f'{varname} = {value} ')
+        script.close()
+        
+    #The Print Var Function(*Made By IT-Err0r*)
+    def ruby_var_str(varname,value: str):
+        
+        script = open("script.rb","a")
+        script.write(f'{varname} = "{value}" ')
         script.close()
